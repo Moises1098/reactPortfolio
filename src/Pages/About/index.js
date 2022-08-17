6a9e4b80af5ea4f7b2dc8ec23dd6172573ce8c3d
Moises <moises109811@gmail.com>
2022-08-16 17:28:11 -0700

    changed favicon and updated about me

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -7,11 +7,10 @@ const About = ()=>{
                 <Card.Header id="header">About Me</Card.Header>
                 <Card.Body>
                   <Card.Title>Hello, there! My name is Moises Gonzalez,</Card.Title>
-                  <Card.Text> I am a 20 year old first generation college student. I have a triple major in Biology, French and Communications. Future 
-                    jobs entitle an FBI biologist or pathologist and something in Web developing. I am Trilingual, speaking Spanish, English,
-                    and French. Fall 2022 I plan to transfer to UCSD to continue my biology Bachelors Degree. One fun fact would be that I 
-                    train at the San Diego Circus Center, doing handstands, flexibility/contortion, and aerial lyra hoop and silks. I also have 
-                    weekly voice lessons with a teacher I have had since high school. I recently started this coding boot camp at UCSD to learn how to web develop.
+                  <Card.Text> I am a 20 year old first generation college student. I am Trilingual, speaking Spanish, English,
+                    and French. I am a UCSD student and plan to gradute in 2024 with a major in General Biology and a minor in 
+                    Cognitive Science. My future job goal is to become an Astronaut Biologist/Doctor to help my team medically and conduct 
+                    research on other planets.
                   </Card.Text>
                 </Card.Body>
             </Card>
