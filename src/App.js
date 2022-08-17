f2d27283af4d7a4df7b1b142f5cc2362e7e4e4ed
Moises <moises109811@gmail.com>
2022-08-16 23:30:41 -0700

    updated tabs

--- src/App.js
+++ src/App.js
@@ -7,27 +7,40 @@ import Work from './Pages/Work';
 import Resume from './Pages/Resume';
 import Home from './Pages/Home';
 
-
-function App() {
-  const [page, setPage] = useState('home')
-  const Render = () => {
-    switch(page){
-      case 'home':
-      return <Home />
-      case 'contact':
-      return <ContactMe />
-      case 'work':
-      return <Work />
-      case 'resume':
-      return <Resume />
-    }
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  return (
-    <div className="App">
-      <Navigation page={page} setPage={setPage}/>
-      <Render />
-    </div>
-  );
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
+
+// function App() {
+//   const [page, setPage] = useState('home')
+//   const Render = () => {
+//     switch(page){
+//       case 'home':
+//       return <Home />
+//       case 'contact':
+//       return <ContactMe />
+//       case 'work':
+//       return <Work />
+//       case 'resume':
+//       return <Resume />
+//     }
+//   }
+//   return (
+//     <div className="App">
+//       <Navigation page={page} setPage={setPage}/>
+//       <Render />
+//     </div>
+//   );
+// }
 
 export default App;
