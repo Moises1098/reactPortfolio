5a8b23bf5301525cae867eda383a2c6757366fa5
Moises <moises109811@gmail.com>
2022-08-17 00:11:29 -0700

    fixes

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -9,53 +9,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navigation = (props) => { 
     const clickHandler = (event) => {
-      const id = event.target.id
-      props.setPage(id)
+        const id = event.target.id
+        props.setPage(id)
     }
     return ( 
-      
 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 <Card.Img variant="left" src={ProfilePicture} height="100" />
       <Navbar.Brand>Moises Gonzalez</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link class="tablinks" onClick="openCity(event, 'Home')" href="home" id="home">Home</Nav.Link>
-          <Nav.Link class="tablinks" onClick="openCity(event, 'About')" href="about" id="about">About Me</Nav.Link>
-          <Nav.Link class="tablinks" onClick="openCity(event, 'Contact')" href="contact" id="contact">Contact Me</Nav.Link>
-          <Nav.Link class="tablinks" onClick="openCity(event, 'Work')" href="work" id="work">Work</Nav.Link>
-          <Nav.Link class="tablinks" onClick="openCity(event, 'Resume')" href="resume" id="resume">Resume</Nav.Link>
+          <Nav.Link onClick={clickHandler}href="home" id="home">Home</Nav.Link>
+          <Nav.Link onClick={clickHandler}href="about" id="about">About Me</Nav.Link>
+          <Nav.Link onClick={clickHandler}href="contact" id="contact">Contact Me</Nav.Link>
+          <Nav.Link onClick={clickHandler}href="work" id="work">Work</Nav.Link>
+          <Nav.Link onClick={clickHandler}href="resume" id="resume">Resume</Nav.Link>
         </Nav>
       </Navbar.Collapse>
-      <div>
-
-<h3>Fade in Tabs</h3>
-
-<div class="tab">
-  <button class="tablinks" onclick="openCity(event, 'London')">London</button>
-  <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
-  <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
-</div>
-
-<div id="London" class="tabcontent">
-  <h3>London</h3>
-  <p>London is the capital city of England.</p>
-</div>
-
-<div id="Paris" class="tabcontent">
-  <h3>Paris</h3>
-  <p>Paris is the capital of France.</p> 
-</div>
-
-<div id="Tokyo" class="tabcontent">
-  <h3>Tokyo</h3>
-  <p>Tokyo is the capital of Japan.</p>
-</div>
-
-
-      </div>
-  </Navbar>
-    
+    </Navbar>
     )  
 }
 export default Navigation
