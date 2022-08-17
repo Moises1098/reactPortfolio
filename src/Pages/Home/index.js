371c890e5262947b51b3a11972b863010b2de923
Moises <moises109811@gmail.com>
2022-08-16 22:46:20 -0700

    added home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,14 @@
 import './home.css'
 const Home = () => {
     return (
-        `<h1>Hi! I'm Moises Gonzalez</h1>
-        <h2>Welcome to my online portfolio</h2>`
+        <div>
+        <div class="main">
+        <h1>Hi! I'm Moises Gonzalez</h1>
+        <h2>Welcome to my online portfolio</h2>
+    </div>
+
+    <div class="right"> <h1>Skills</h1> </div>
+    </div>
     )
 }
 
