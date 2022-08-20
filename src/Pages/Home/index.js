b46db4fdd5f6135360e4d5c90a6e0ff45356934d
Moises <moises109811@gmail.com>
2022-08-20 12:28:41 -0700

    added 1 skill

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,9 @@
 import './home.css'
+import ListGroup from 'react-bootstrap/ListGroup';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 const Home = () => {
     return (
     <div>
@@ -7,9 +12,36 @@ const Home = () => {
         <h2>Welcome to my online portfolio</h2>
         </div>
 
-        <div class="right"> <h1>Skills</h1> </div>
+        <div class="right"> 
+        <h1>Skills</h1> 
+        <ListGroup variant="flush">
+        <ListGroup.Item>
+        <Button variant="primary" onClick={handleShow}>
+        Web Developer
+       </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Computer Languages</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>HTML, CSS, JAVASCRIPT, HANDLEBARS, REACT, MONGODB, MYSQL, BOOTSTRAP</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Exit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+        </ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+        </ListGroup>
+        </div>
+
     </div>
     )
+
+
 }
 
 export default Home
