389363c66bc0633b6d8a9852ae140984d3204479
Moises <moises109811@gmail.com>
2022-08-23 20:22:04 -0700

    added skills

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,21 @@
 import './home.css'
 import Button from 'react-bootstrap/Button';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Popover from 'react-bootstrap/Popover';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { ModalBody } from 'react-bootstrap';
+
 
 const Home = () => {
+  const [show, setShow] = useState(false);
 
+  const handleCloseWebDev = () => setShow(false);
+  const handleShowWebDev = () => setShow(true);
+
+  const handleCloseCA = () => setShow(false);
+  const handleShowCA = () => setShow(true);
     return (
+      
     <div>
         <div className='main'>
         <h1>Hi! I'm Moises Gonzalez</h1>
@@ -14,79 +24,60 @@ const Home = () => {
 
         <div class="right"> 
         <h1>Skills</h1> 
-
- 
-
-
-
-
-
-
-
-
-
-
-
-    
-        <Button variant="primary" onClick={handleShow}>
+        <Button variant="primary" onClick={handleShowWebDev}>
         Web Developer
-       </Button>
-      <Modal show={show} onHide={handleClose}>
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleCloseWebDev}
+        backdrop="static"
+        keyboard={false}
+      >
         <Modal.Header closeButton>
           <Modal.Title>Computer Languages</Modal.Title>
         </Modal.Header>
-        <Modal.Body>HTML, CSS, JAVASCRIPT, HANDLEBARS, REACT, MONGODB, MYSQL, BOOTSTRAP</Modal.Body>
+        <Modal.Body>
+        HTML, CSS, JAVASCRIPT, HANDLEBARS, REACT, MONGODB, MYSQL, BOOTSTRAP
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Exit
+          <Button variant="secondary" onClick={handleCloseWebDev}>
+            Close
           </Button>
+          <Button variant="primary">Understood</Button>
         </Modal.Footer>
       </Modal>
-      
-        <Button variant="primary" onClick={handleShow}>
-        Education
-       </Button>
-      <Modal show={show} onHide={handleClose}>
+
+      <Button variant="primary" onClick={handleShowCA}>
+        Extracurricular
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleCloseCA}
+        backdrop="static"
+        keyboard={false}
+      >
         <Modal.Header closeButton>
-          <Modal.Title>Degrees</Modal.Title>
+          <Modal.Title>Circus Arts</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-            Asociates in Arts: Communication, French, Liberal Arts: Mathematics and Science, and Liberal Arts: Emphasis from Southwester College
-           June 2019 - June 2022
-            <br> 
+        <Modal.Body>Aerial lyra (hoop), Aerial silks and trapeze</Modal.Body>
+        
+        <Modal.Title>Music</Modal.Title>
+        <Modal.Body>Piano, Voice, Ukulele, Guitar, Violin, and can read Sheet Music</Modal.Body>
 
-            </br>
-            Bachelors in Science: Biology with a minor in Cognitive Science in progess at UC San Diego
-            June 2022 - Present
-        </Modal.Body>
-        <Modal.Header>
-          <Modal.Title>Certificates</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-           UCSD Web Development Bootcamp
-           Novemer 2021 - May 2022
-           <br>
-           </br>
-           LEADR Program- Scripps Journal Club
-           March 2022 -  April 2022
-        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Exit
+          <Button variant="secondary" onClick={handleCloseCA}>
+            Close
           </Button>
+          <Button variant="primary">Understood</Button>
         </Modal.Footer>
       </Modal>
-     
-
-        <ListGroup.Item>Expirence</ListGroup.Item>
-        <ListGroup.Item>Extra Curricualars</ListGroup.Item>
-    
-        </div>
-
+      </div>
     </div>
    
  
-    )
+  )
 
 
 }
