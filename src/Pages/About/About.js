93bf0a1eed226e134b855c848c256fec147c38cc
Moises <moises109811@gmail.com>
2022-11-27 16:56:18 -0800

    fixed other tabs

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,19 +1,10 @@
-import Card from "react-bootstrap/Card"
+import React from "react"
 import './About.css'
 const About = ()=>{
     return (
         <div class="aboutMe">
-            <Card border="secondary">
-                <Card.Header id="header">About Me</Card.Header>
-                <Card.Body>
-                  <Card.Title>Hello, there! My name is Moises Gonzalez,</Card.Title>
-                  <Card.Text> I am a 20 year old first generation college student. I am Trilingual, speaking Spanish, English,
-                    and French. I am a UCSD student and plan to gradute in 2024 with a major in General Biology and a minor in 
-                    Cognitive Science. My future job goal is to become an Astronaut Biologist/Doctor to help my team medically and conduct 
-                    research on other planets.
-                  </Card.Text>
-                </Card.Body>
-            </Card>
+           <header>About Me</header>
+           <h2>Hi, my name is Moises Gonzalez. I am student at UCSD working towards a dual bacholers degree in General Biology and Global Health. My plans after graducation is to work on an MD/PhD docteraate degree as well as continuing to work on full stack applications as a web developer.</h2>
         </div>
     )
 }
