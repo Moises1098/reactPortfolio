b976b212bffdb698a9e7f9b78761b3388b646a2d
Moises <moises109811@gmail.com>
2022-11-28 10:23:11 -0800

    removed classname

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -6,24 +6,24 @@ export default function NavBar() {
   return <nav className="nav">
     <Link to="/" className="nav-name">Moises Gonzalez</Link>
     <ul>
-      <CustomLink className="nav-tab" to="/home">Home</CustomLink>
-      <CustomLink className="nav-tab" to="/about">About</CustomLink>
-      <CustomLink className="nav-tab" to="/contact">Contact</CustomLink>
-      <CustomLink className="nav-tab" to="/work">Work</CustomLink>
-      <CustomLink className="nav-tab" to="/resume">Resume</CustomLink>
+      <CustomLink to="/home">Home</CustomLink>
+      <CustomLink to="/about">About</CustomLink>
+      <CustomLink to="/contact">Contact</CustomLink>
+      <CustomLink to="/work">Work</CustomLink>
+      <CustomLink to="/resume">Resume</CustomLink>
     </ul>
   </nav>
 }
 
 function CustomLink({ to, children, ...props }) {
   const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch ({ path: resolvedPath.pathname, end: true})
-  
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
   return (
     <li className={isActive === to ? "active" : ""}>
       <Link to={to} {...props}>
         {children}
-        </Link>
+      </Link>
     </li>
   )
 }
