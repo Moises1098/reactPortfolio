b88132770a101f2496eebd0e60ceb7aba7d6929a
Moises <moises109811@gmail.com>
2022-11-28 16:30:45 -0800

    added nav bar

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -1,18 +1,67 @@
 import './navBar.css'
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBCollapse,
+  MDBNavbar,
+  MDBNavbarToggler,
+} from 'mdb-react-ui-kit';
 
 export default function NavBar() {
-  return <nav className="nav">
-    <Link to="/" className="nav-name">Moises Gonzalez</Link>
-    <ul>
-      <CustomLink to="/home">Home</CustomLink>
-      <CustomLink to="/about">About</CustomLink>
-      <CustomLink to="/contact">Contact</CustomLink>
-      <CustomLink to="/work">Work</CustomLink>
-      <CustomLink to="/resume">Resume</CustomLink>
-    </ul>
-  </nav>
+  const [showAnimated2, setShowAnimated2] = useState(false);
+  return (
+    <>
+      <section className='HambarMenu'>
+        <MDBNavbar>
+          <MDBContainer fluid>
+            <Link to="/" className="nav-name">Moises Gonzalez</Link>
+            <MDBNavbarToggler
+              type='button'
+              className='second-button'
+              data-target='#navbarToggleExternalContent'
+              aria-controls='navbarToggleExternalContent'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+              onClick={() => setShowAnimated2(!showAnimated2)}
+            >
+              <div className={`animated-icon2 ${showAnimated2 && 'open'}`}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </MDBNavbarToggler>
+            <div className='navbar-links two'>
+              <ul>
+                <CustomLink to="/home">Home</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+                <CustomLink to="/contact">Contact</CustomLink>
+                <CustomLink to="/work">Work</CustomLink>
+                <CustomLink to="/resume">Resume</CustomLink>
+              </ul>
+            </div>
+          </MDBContainer>
+        </MDBNavbar>
+
+        <MDBCollapse show={showAnimated2}>
+            <div className='navbar-links three'>
+              <ul>
+                <CustomLink to="/home">Home</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+                <CustomLink to="/contact">Contact</CustomLink>
+                <CustomLink to="/work">Work</CustomLink>
+                <CustomLink to="/resume">Resume</CustomLink>
+              </ul>
+            </div>
+        </MDBCollapse>
+      </section>
+
+
+
+    </>
+    
+  )
 }
 
 function CustomLink({ to, children, ...props }) {
@@ -20,7 +69,7 @@ function CustomLink({ to, children, ...props }) {
   const isActive = useMatch({ path: resolvedPath.pathname, end: true })
 
   return (
-    <li className={isActive === to ? "active" : ""}>
+    <li className={isActive ? "active" : ""}>
       <Link to={to} {...props}>
         {children}
       </Link>
